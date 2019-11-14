3b8b4f3fe66379521f2ed147e3efd2f60032501b
Arely Guevara <arelyguevara@Administrators-MacBook-Pro.local>
2019-11-14 12:55:21 -0500

    Updates

---
+++ precache-manifest.17ebc613039e6c280bc48bfd3739dcfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c744d09b4e346890c2347d3ad1059f9b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d6ad54d0d4e32dada52e",
+    "url": "/portfolio/static/css/2.becd5eea.chunk.css"
+  },
+  {
+    "revision": "cfa596f48f27eace06e5",
+    "url": "/portfolio/static/css/main.7d91b40d.chunk.css"
+  },
+  {
+    "revision": "d6ad54d0d4e32dada52e",
+    "url": "/portfolio/static/js/2.a1715527.chunk.js"
+  },
+  {
+    "revision": "cfa596f48f27eace06e5",
+    "url": "/portfolio/static/js/main.4128fc68.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "7338a1f0549c5d632846a30f7dccd1d7",
+    "url": "/portfolio/static/media/resume.7338a1f0.pdf"
+  }
+]);
